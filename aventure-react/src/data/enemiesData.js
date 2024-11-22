2b2b4317313ce47be66ff1c25e0c64f45480aca7
GoutteDooo <goudal.Donovan@live.fr>
2024-11-22 23:41:15 +0100

    add enemiesData.js

---
+++ aventure-react/src/data/enemiesData.js
@@ -0,0 +1,17 @@
+// src/data/enemiesData.js
+const enemiesData = {
+  wolf: {
+    name: "Epouvantail",
+    health: 20,
+    attack: 0,
+    defense: 0,
+  },
+  goblin: {
+    name: "Gobelin rusé",
+    health: 25,
+    attack: 5,
+    defense: 2,
+  },
+};
+
+export default enemiesData;
--- aventure-react/src/pages/Adventure.jsx
+++ aventure-react/src/pages/Adventure.jsx
@@ -1,12 +1,13 @@
 import HUD from '../components/HUD';
+import AdventurePage from '../components/AdventurePage';
 
 function Adventure() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-4">Bienvenue dans l'aventure !</h1>
+      <h1 className="text-3xl font-bold mb-4">Aventure</h1>
       <HUD />
       <div className="mt-8">
-        <p>Voici le début de votre quête...</p>
+        <AdventurePage />
         {/* Ajoute ici les choix narratifs plus tard */}
       </div>
     </div>
