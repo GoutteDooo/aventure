4ee1fa6423bcfe3dd538619259f9cb71f800b3ff
GoutteDooo <goudal.Donovan@live.fr>
2024-11-24 19:11:54 +0100

    feat: combat, stats player added

--- aventure-react/src/components/Combat.jsx
+++ aventure-react/src/components/Combat.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Combat = ({enemy},{player}) => {
+const Combat = ({enemy}) => {
+  const [playerStats, setPlayerStats] = useState(null);
+
+  //Charger les données du joueur depuis le localStorage
+  useEffect(() => {
+    const storedPlayerData = localStorage.getItem("playerData");
+    if (storedPlayerData) {
+      setPlayerStats(JSON.parse(storedPlayerData));
+    }
+  }, [])
+
+  if (!playerStats) {
+    return <p className='animate-pulsing animate-iteration-count-infinite'>Chargement des données du joueur...</p>
+  }
+
     return (
         <div className="combat">
           <h2>Combat contre {enemy.name}</h2>
           <p>Vie de l'ennemi : {enemy.health}</p>
-          <p>Votre vie : VIE DU JOUEUR</p>
+          <p>Votre vie : {playerStats.stats.health}</p>
           <button onClick={""}>Attaquer</button>
         </div>
     );
