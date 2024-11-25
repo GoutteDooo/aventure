391295483e2f663d9e4e0c454ccc337f31286f80
GoutteDooo <goudal.Donovan@live.fr>
2024-11-25 22:35:54 +0100

    style: combat visuel added

--- aventure-react/src/components/Combat.jsx
+++ aventure-react/src/components/Combat.jsx
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 const Combat = ({enemy}) => {
   const [playerStats, setPlayerStats] = useState(null);
+  const [playerTurn, setPlayerTurn] = useState(false); //vérifie si joueur a l'initiative avant de pouvoir attaquer
+
+  const handlePlayerTurn = () => {
+    if (playerTurn) {
+      return;
+    }
+  }
 
   //Charger les données du joueur depuis le localStorage
   useEffect(() => {
@@ -16,14 +23,30 @@ const Combat = ({enemy}) => {
   }
 
     return (
+      <div className="combat-container">
         <div className="combat">
-          <div className="combat__ennemi-stats">
-            <h2>{enemy.name}</h2>
-            <p>Vie de l'ennemi : {enemy.health}</p>
+          <div className={`combat__player__stats ${playerTurn ? "combat__play" : "combat__wait"}`}>
+            <div className='combat__player__stats--name'>{playerStats.name}</div>
+            <p>Vie : {playerStats.stats.health}</p>
+            <p>Attaque : {playerStats.stats.attack}</p>
+            <p>Défense : {playerStats.stats.defense}</p>
+            <p>Adresse : {playerStats.stats.accuracy}</p>
+            <p>Initiative : {playerStats.stats.initiative}</p>
           </div>
-          <p>Votre vie : {playerStats.stats.health}</p>
-          <button onClick={""}>Attaquer</button>
+          <div className={`combat__ennemy__stats ${playerTurn ? "combat__ennemy__wait" : "combat__ennemy__play"}`}>
+            <div className="combat__ennemy__stats--name">
+              {enemy.name}
+            </div>
+            <p>Vie : {enemy.health}</p>
+            <p>Attaque : {enemy.attack}</p>
+            <p>Defense : {enemy.defense}</p>
+            </div>
         </div>
+        {/* Si tour du joueur, alors afficher*/}
+        {playerTurn && (
+          <div className="your-turn">C'est votre tour</div>
+        )}
+      </div>
     );
 };
 
