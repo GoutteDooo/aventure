aa273acffb0416a861a08a30f49d9a641e5b33b6
gouttedooo <goudal.donovan@live.fr>
2024-11-28 16:33:26 +0100

    style: fenetre de loot en cours de développement

--- aventure-react/src/components/Loot.jsx
+++ aventure-react/src/components/Loot.jsx
@@ -1,20 +1,23 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React from "react";
 
-const Loot = ({loots, onClose}) => {
-
-    return (
-        <div className='loots'>
-            <div className="loots__drop">
-            <h1>Récupéré : </h1>
-            <ul>
-                {loots.map((loot, index) => (
-                    <li key={index}>{loot}</li>
-                ))}
-            </ul>
-            </div>
-            <button className="loots__next" onClick={onClose}>Etape suivante</button>
+const Loot = ({ loots, onClose }) => {
+  return (
+    <div className="bg__loots">
+      <div className="loots">
+        <div className="loots__drop">
+          <h1>Récupéré : </h1>
+          <ul>
+            {loots.map((loot, index) => (
+              <li key={index}>{loot}</li>
+            ))}
+          </ul>
         </div>
-    );
+        <button className="loots__next" onClick={onClose}>
+          Etape suivante
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Loot;
