2b042ef13a4f36126650ba9689c2f7b64781552d
GoutteDooo <goudal.Donovan@live.fr>
2024-11-28 23:01:38 +0100

    feat: bouton restart added

--- aventure-react/src/pages/Status.jsx
+++ aventure-react/src/pages/Status.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Inventaire from '../components/Inventaire';
 import { useNavigate } from 'react-router-dom';
 
 const Status = () => {
     const navigate = useNavigate();
+    const currentStep = localStorage.getItem("currentStepId");
+    const [currentStepId, setCurrentStepId] = useState(() => {
+        return currentStep ? JSON.parse(currentStep) : 1;
+    });
+
+    useEffect(() => {
+        //Met à jour le localStorage lorsque player a cliqué sur bouton
+        localStorage.setItem("currentStepId", JSON.stringify(currentStepId));
+        //A faire également pour les datas du joueur
+      }, [currentStepId]);
+    
     return (
         <div className='status'>
             <Inventaire />
             <button className="status__return" onClick={() => navigate("/adventure")}>Retourner à l'aventure</button>
+            <button className='restartButton' onClick={() => setCurrentStepId(1)}>
+                Effacer sa mémoire (retour au début)
+            </button>
         </div>
     );
 };
