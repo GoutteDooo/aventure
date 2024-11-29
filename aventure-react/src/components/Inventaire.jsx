8a0bb3f7d2118229f82ba99582cfbf121b85fb14
gouttedooo <goudal.donovan@live.fr>
2024-11-29 18:38:50 +0100

    style: inventaire display

--- aventure-react/src/components/Inventaire.jsx
+++ aventure-react/src/components/Inventaire.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Inventaire = () => {
-    const [inventaire, setInventaire] = useState(false);
-    return (
-        <div className='inventory'>
-            INVENTAIRE
-        </div>
-    );
+  const [inventaire, setInventaire] = useState(() => {
+    const savedInventory = localStorage.getItem("inventory");
+    return savedInventory
+      ? JSON.parse(savedInventory)
+      : ["", "", "", "", "", ""];
+  });
+
+  return (
+    <div className="inventory">
+      <div className="inventory__container">
+        {inventaire &&
+          inventaire.map((item, index) => (
+            <div key={index} className="inventory__container__item">
+              {item}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Inventaire;
