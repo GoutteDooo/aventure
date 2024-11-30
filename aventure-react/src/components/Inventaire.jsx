2e87b576dff6aed43a0fc8ef152ba87f1e58ec1e
GoutteDooo <goudal.Donovan@live.fr>
2024-11-30 11:51:33 +0100

    chore: ajout items inventory and desc

--- aventure-react/src/components/Inventaire.jsx
+++ aventure-react/src/components/Inventaire.jsx
@@ -1,22 +1,47 @@
-import React, { useState } from "react";
+import React, { act, useState } from "react";
+import itemsData from "../data/itemsData";
 
 const Inventaire = () => {
   const [inventaire, setInventaire] = useState(() => {
     const savedInventory = localStorage.getItem("inventory");
     return savedInventory
       ? JSON.parse(savedInventory)
-      : ["", "", "", "", "", ""];
+      : ["Sandwich à l'ail", "Potion de santé", "", "", "", ""];
   });
+
+  const [activeItem, setActiveItem] = useState(null);
+  
+  const handleItemActive = (targetItemText) => {
+    // const itemClicked = itemsData.find((item) => target === item.name);
+    const itemClicked = targetItemText;
+    setActiveItem(itemClicked);
+    console.log(itemClicked);
+    
+    
+  }
 
   return (
     <div className="inventory">
       <div className="inventory__container">
         {inventaire &&
           inventaire.map((item, index) => (
-            <div key={index} className="inventory__container__item">
+            <div key={index} className={`inventory__container__item ${item ? "inventory__container__item--active" : ""}`} onClick={(e) => handleItemActive(e.target.textContent)}>
               {item}
             </div>
           ))}
+      </div>
+      <div className="inventory__description">
+        {activeItem ? (<>
+            <div className="inventory__description--useless">
+                {itemsData.find((item) => item.name === activeItem).desc_useless}
+        </div>
+            <div className="inventory__description--use">
+                {itemsData.find((item) => item.name === activeItem).desc_use}
+        </div>
+        </>
+    ) : (
+    <></>
+    )}
       </div>
     </div>
   );
--- aventure-react/src/data/itemsData.js
+++ aventure-react/src/data/itemsData.js
@@ -4,24 +4,42 @@ const itemsData = [
     name: "Potion de santé",
     effect: "heal",
     value: 20,
+    desc_useless:
+      "Une vieille fiole poussiéreuse contenant un liquide verdâtre foncé.",
+    desc_use: "Soigne 100% des HP",
   },
   {
     id: 2,
     name: "Chapeau de paille",
     effect: "chance",
     value: 3,
+    desc_useless: "",
+    desc_use: "",
   },
   {
     id: 3,
     name: "Tenue de paysan",
     effect: "defense",
     value: 1,
+    desc_useless: "",
+    desc_use: "",
   },
   {
     id: 4,
     name: "Bâton en bois",
     effect: "attack",
     value: 1,
+    desc_useless: "",
+    desc_use: "",
+  },
+  {
+    id: 5,
+    name: "Sandwich à l'ail",
+    effect: "heal",
+    value: 100,
+    desc_useless:
+      "Un délicieux sandwich confectionné avec soin par votre épouse.",
+    desc_use: "Soigne 100 HP",
   },
 ];
 
